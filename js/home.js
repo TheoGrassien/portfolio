9d40af8c14dcfc2f0801e1b4b461810f3f87f796
Théo Grassien <theo.grassien@gmail.com>
2023-01-11 15:25:06 +0100

    sticky gallery

--- js/home.js
+++ js/home.js
@@ -58,141 +58,37 @@ window.addEventListener("scroll", () => {
 });
 
 // Sticky gallery horizontal scroll effects
-// let gallery = document.querySelector(".gallery");
-// let galleryContainer = document.querySelector(".gallery-container");
-// let gallerySticky = document.querySelector(".gallery-sticky");
-// let lastImage = document.querySelector(".gallery img:last-of-type");
-
-// function stickyGallery() {
-//   window.addEventListener("load", () => {
-//     let initialGalleryTopDist;
-//     let scrollDistance;
-//     let galleryWidth = gallery.scrollWidth;
-//     let windowWidth = window.innerWidth;
-
-//     initialGalleryTopDist = galleryContainer.getBoundingClientRect().top;
-//     scrollDistance = initialGalleryTopDist + galleryWidth - windowWidth;
-//     galleryContainer.style.height = galleryWidth + "px";
-
-//     window.addEventListener("scroll", function () {
-//       var scrollTop = window.scrollY;
-
-//       if (scrollTop >= initialGalleryTopDist && scrollTop <= scrollDistance) {
-//         gallery.style.transform =
-//           "translateX(-" + (scrollTop - initialGalleryTopDist) + "px)";
-//       }
-//     });
-//   });
-// }
-
-/**
- * By Alvaro Trigo
- * Follow me on Twitter: https://twitter.com/imac2
- */
-// (function () {
-//   init();
-
-//   var g_containerInViewport;
-//   function init() {
-//     setStickyContainersSize();
-//     bindEvents();
-//   }
-
-//   function bindEvents() {
-//     window.addEventListener("wheel", wheelHandler);
-//   }
-
-//   function setStickyContainersSize() {
-//     document
-//       .querySelectorAll(".gallery-container")
-//       .forEach(function (container) {
-//         const stikyContainerHeight =
-//           container.querySelector(".gallery-sticky").scrollWidth;
-//         container.setAttribute(
-//           "style",
-//           "height: " + stikyContainerHeight + "px"
-//         );
-//       });
-//   }
-
-//   function isElementInViewport(el) {
-//     const rect = el.getBoundingClientRect();
-//     return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
-//   }
-
-//   function wheelHandler(evt) {
-//     const containerInViewPort = Array.from(
-//       document.querySelectorAll(".gallery-container")
-//     ).filter(function (container) {
-//       return isElementInViewport(container);
-//     })[0];
-
-//     if (!containerInViewPort) {
-//       return;
-//     }
-
-//     var isPlaceHolderBelowTop =
-//       containerInViewPort.offsetTop < document.documentElement.scrollTop;
-//     var isPlaceHolderBelowBottom =
-//       containerInViewPort.offsetTop + containerInViewPort.offsetHeight >
-//       document.documentElement.scrollTop;
-//     let g_canScrollHorizontally =
-//       isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
-
-//     if (g_canScrollHorizontally) {
-//       containerInViewPort.querySelector(".gallery-sticky").scrollLeft +=
-//         evt.deltaY;
-//     }
-//   }
-// })();
-
-// const scrollContainer = document.querySelector(".gallery-sticky");
-
-// scrollContainer.addEventListener("wheel", (evt) => {
-//   evt.preventDefault();
-//   // console.log(evt.deltaY);
-//   scrollContainer.scrollLeft += evt.deltaY;
-// });
-
-// Switch gallery to horizontal scroll when is touch device
-// if (touchDevice()) {
-//   gallerySticky.classList.add("touch");
-// } else {
-//   stickyGallery();
-// }
-
-// gsap.to(".gallery-sticky", {
-//   xPercent: -100,
-//   x: () => innerWidth,
-//   ease: "none",
-//   scrollTrigger: {
-//     trigger: ".gallery-sticky",
-//     start: "top top",
-//     end: () => innerWidth * 3,
-//     scrub: true,
-//     pin: true,
-//     invalidateOnRefresh: true,
-//     anticipatePin: 1,
-//   },
-// });
-// console.log(gallerySticky.offsetWidth);
-
 gsap.registerPlugin(ScrollTrigger);
-let sections = gsap.utils.toArray(".gallery img");
+let images = gsap.utils.toArray(".gallery img");
 let gallery = document.querySelector(".gallery");
 
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none", // <-- IMPORTANT!
-  scrollTrigger: {
-    start: "top 104px",
-    trigger: ".gallery",
+function getMaxWidth() {
+  maxWidth = 0;
+  images.forEach((image) => {
+    maxWidth += image.offsetWidth;
+  });
+  maxWidth += 48 * (images.length - 1);
+  return maxWidth;
+}
+getMaxWidth();
 
-    pin: true,
-    scrub: true,
-    // markers: true,
+// Activate the effect only when is not touche device
+if (touchDevice()) {
+  gallery.classList.add("touch");
+} else {
+  gsap.to(images, {
+    x: () => `-${getMaxWidth() - window.innerWidth}`,
+    ease: "none",
+    scrollTrigger: {
+      start: "top 104px",
+      trigger: ".gallery",
 
-    // snap: directionalSnap(1 / (sections.length - 1)),
-    end: `+=${gallery.scrollWidth}`,
-  },
-});
+      pin: true,
+      scrub: true,
+      // markers: true,
+      invalidateOnRefresh: true,
+
+      end: () => `+=${getMaxWidth()}`,
+    },
+  });
+}
